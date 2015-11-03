3ca0b3472a5fdae38e29196106f38e3bb0cf30ba
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-03 11:51:02 -0800

    add dist

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _srcIndex = require('./src/index');
+
+var _srcIndex2 = _interopRequireDefault(_srcIndex);
+
+var aureliaInterface = new _srcIndex2['default']();
+var ai = aureliaInterface;
+exports.ai = ai;
+exports.aureliaInterface = aureliaInterface;
+//# sourceMappingURL=index.js.map
---
+++ dist/src/index.js
@@ -0,0 +1,51 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var cwd = _path2['default'].join.bind(_path2['default']);
+
+var AureliaInterface = (function () {
+  function AureliaInterface() {
+    _classCallCheck(this, AureliaInterface);
+
+    this.path = cwd('jspm_packages/github/aurelia/interface@master/dist/sass');
+    this.plugins = [this.path];
+  }
+
+  _createClass(AureliaInterface, [{
+    key: 'includePaths',
+    value: function includePaths() {
+      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      return this.plugins.concat(args);
+    }
+  }, {
+    key: 'with',
+    value: function _with() {
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+
+      return this.plugins.concat(args);
+    }
+  }]);
+
+  return AureliaInterface;
+})();
+
+exports['default'] = AureliaInterface;
+module.exports = exports['default'];
+//# sourceMappingURL=index.js.map
